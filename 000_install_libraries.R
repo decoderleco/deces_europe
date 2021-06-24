819d39454b38e4082134fb149213861d6ed34ac6
JeanGarf <thierryw777@gmail.com>
2021-06-24 02:01:20 +0200

    Extract libraries install in 000_install_libraries.R

---
+++ 000_install_libraries.R
@@ -0,0 +1,31 @@
+# Ce fichier n'est à executer qu'une seule fois
+
+# Installer le package eurostat et ses copains
+install.packages("eurostat")
+install.packages("maptools")
+install.packages("rgdal")
+install.packages("maps");
+install.packages("leaflet");
+install.packages(("questionr"));
+install.packages("ggplot2");
+install.packages("rnaturalearth")
+install.packages("rnaturalearthdata")
+install.packages("rgeos")
+install.packages("readr")
+install.packages("lsr")
+
+install.packages("pyramid")
+install.packages("reshape2")
+install.packages("tidyr")
+
+install.packages("igraph")
+
+
+# Pour la generation de l'article
+install.packages("knitr")
+install.packages("rmarkdown")
+install.packages("markdown")
+
+library(knitr)
+library(rmarkdown)
+library(markdown)
--- 100_article_rmarkdown.R
+++
@@ -1,7 +0,0 @@
-install.packages("knitr")
-install.packages("rmarkdown")
-install.packages("markdown")
-
-library(knitr)
-library(rmarkdown)
-library(markdown)
