c299dd3e71174f0060bfaeaaba19f9c9ec76a38f
JeanGarf <thierryw777@gmail.com>
2021-07-11 20:00:31 +0200

    #25 Script pour tout enchainer

---
+++ 002_run_all.R
@@ -0,0 +1,13 @@
+# TODO: Add comment
+# 
+# Author: JeanGarf
+###############################################################################
+
+
+source("005_functions.R")
+source("010_creation_tables_deces_europe.R")
+source("020_analyses_eurostat.R")
+source("030_analyse_deces_hebdomadaires.R")
+source("040_deces_francais.R")
+source("050_delivrance_medicaments.R")
+#source("060_SIRD.R")
