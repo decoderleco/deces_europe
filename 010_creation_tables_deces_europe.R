ffad0e9d0d54b33526dc6fd9f7fbc95f1a022a60
JeanGarf <thierryw777@gmail.com>
2021-08-04 21:58:12 +0200

    Deux petites corrections

--- 010_creation_tables_deces_europe.R
+++ 010_creation_tables_deces_europe.R
@@ -57,7 +57,11 @@ a__original_es_deces_annuel_le2019 <- a__f_downloadEuroStatIfNeeded(var = a__ori
 # sex
 # values = population dans cette tranche d'âge à être décédée
 a__original_es_deces_week <- a__f_downloadEuroStatIfNeeded(var = a__original_es_deces_week, 
-		euroStatFileName = "demo_r_mwk_05")
+		euroStatFileName = "demo_r_mwk_05") %>%
+		# Trier les lignes
+		arrange(geo, sex, time, age) %>%
+		# Reorganiser les colonnes
+		select(geo, sex, time, age, everything())
 
 
 ################################################################################
@@ -148,6 +152,10 @@ b__es_deces_et_pop_par_annee <- b__es_deces_et_pop_par_annee %>%
 es_deces_annuel_age <- b__es_deces_et_pop_par_annee %>% 
 		filter(age !="Y_LT1") %>% 
 		filter(age !="Y_OPEN") %>%
+		filter(str_sub(geo,1,2)!="EU") %>%
+		filter(str_sub(geo,1,2)!="EA") %>% 
+		filter(str_sub(geo,1,3)!="EEA") %>% 
+		filter(str_sub(geo,1,3)!="EFT") %>% 
 		mutate(age = as.double(str_sub(age, 2, length(age)))) 
 
 # Age max des deces par population, année de recensement
