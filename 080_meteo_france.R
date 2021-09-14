5871d10e65bed834ec33a7cd77ca12d51b3eb7ef
unknown <decoderleco.com>
2021-09-14 19:31:06 +0200

    correction

--- 080_meteo_france.R
+++ 080_meteo_france.R
@@ -1,3 +1,5 @@
+library(lubridate)
+
 #### téléchargement traitement des données de météo france pour chercher une relation température / mortalité
 
 #### - https://public.opendatasoft.com/explore/dataset/donnees-synop-essentielles-omm/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B
@@ -8,7 +10,12 @@ saveRDS(meteo,file='C:/Users/xxx/Documents/R/deces_europe/gen/rds/meteo.rds')
 
 meteo<-readRDS('C:/Users/xxx/Documents/R/deces_europe/gen/rds/meteo.rds')
 
-meteo_simple <- meteo %>% select (TempÃ.rature, department..name.,Date)
-meteo_simple <- meteo_simple %>%  group_by(department..name.,Date) %>% 
-  mutate (temperature = mean(TempÃ.rature)) %>% 
-  distinct(department..name.,Date)
+meteo_simple <- meteo %>% select (TempÃ.rature, department..code.,Date)
+meteo_simple <- meteo_simple %>% mutate(jour = str_sub(Date,1,10))
+meteo_simple$jour <- as.Date(meteo_simple$jour,'%Y-%m-%d')
+meteo_simple <- meteo_simple %>% drop_na()
+meteo_simple <- meteo_simple %>% filter(TempÃ.rature>100)
+meteo_simple <- meteo_simple %>% mutate(temperature = as.numeric(TempÃ.rature))
+meteo_simple <- meteo_simple %>%  group_by(department..code.,jour) %>% 
+  summarise(temperature = mean(temperature))
+
