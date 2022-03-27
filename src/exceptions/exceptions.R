4cc798e3be2bcb4c4c0319a6522a1b166ad35f3d
JeanGarf <thierryw777@gmail.com>
2022-03-27 16:41:21 +0200

    #87 : *** Explications complémentaires sur la Gestion des Exceptions

--- src/exceptions/exceptions.R
+++ src/exceptions/exceptions.R
@@ -1,5 +1,23 @@
-# TODO: Add comment
+# Gestion des Exceptions
 # 
+# Exemple d'utilisation
+#
+# Permet de poursuivre l'exécution malgré un Warning ou une Erreur:
+#
+## tryLog( { } )
+#
+# Arrêter l'exécution en cas de Warning ou d'Erreur:
+#
+## tryCatchLog( {
+## 
+##             a <- 3
+##             b <- 5
+##             print(a/b)
+##             warning("Ceci est un warning")
+##             stop("Ceci est une erreur")
+##         }, 
+## warning = b__f_warning, 
+## error = b__f_error)
 ###############################################################################
 
 # Fonction appelée en cas d'erreur
@@ -11,16 +29,4 @@ a__f_error <- function(e) {
 a__f_warning <- function(e) {
 	message(paste0("ATTENTION : ", e))
 }
-
-# Exemple d'utilisation
-## tryCatchLog( {
-## 
-##             a <- 3
-##             b <- 5
-##             print(a/b)
-##             warning("Ceci est un warning")
-##             stop("Ceci est une erreur")
-##         }, 
-## warning = b__f_warning, 
-## error = b__f_error)
 
