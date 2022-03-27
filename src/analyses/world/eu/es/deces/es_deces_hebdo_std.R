a589e5405bdbdc18769f03eee13896db3bec2b63
JeanGarf <thierryw777@gmail.com>
2022-03-27 13:41:13 +0200

    #88 : *** Indiquer que le tryCatchLog empêche de voir les variables dans le debugger sous Eclipse
    Gérer aussi les Warning

--- src/analyses/world/eu/es/deces/es_deces_hebdo_std.R
+++ src/analyses/world/eu/es/deces/es_deces_hebdo_std.R
@@ -9275,145 +9275,154 @@ a__f_plot_es_deces_hebdo_compare_vaccination_regroupe <- function(es_deces_stand
 # Sous fonction pour l'affichage du graphique
 a__f_plot_es_deces_hebdo_std_annee_juin <- function(nomPays, trancheAge, titleSuffix, deces_hebdo, nbLinesFor2013, shallCumul = FALSE) {
 	
-	#
-	# Créer le répertoire pour stocker le graphique
-	#
-	
-	# Déterminer le répertoire et le nom de la colonne à tracer (= la tranche d'âge à tracer)
-	
-	if (shallCumul) {
-		
-		K_DIR_GEN_IMG_EUROSTAT_DECES_ANNEE_COUPEE_ETE <- a__f_createDir(file.path(K_DIR_GEN_IMG_EUROSTAT, '/Deces/Hebdo/Std/Deces_Annee_coupee_ete_Cumul'))
-		
-		colName <- paste0("cum_deces_", gsub("-", "_", trancheAge))
-		
-	} else {
-		
-		K_DIR_GEN_IMG_EUROSTAT_DECES_ANNEE_COUPEE_ETE <- a__f_createDir(file.path(K_DIR_GEN_IMG_EUROSTAT, '/Deces/Hebdo/Std/Deces_Annee_coupee_ete'))
-		
-		colName <- paste0("deces_standardises_si_pop_2020_", gsub("-", "_", trancheAge))
-	}
-	
-	
-	repertoire <- paste0(K_DIR_GEN_IMG_EUROSTAT_DECES_ANNEE_COUPEE_ETE, "/", trancheAge,"/")
-	a__f_createDir(repertoire)
-	
-	#Nom du fichier png à générer
-	pngFileRelPath <- paste0(repertoire, nomPays, ".png")
-	
-	cat(paste0("Creation image (", pngFileRelPath,")\n"))
-	
-	#
-	# Générer le graphique
-	#
-	
-	# Déterminer l'ordonnée pour les labels des saisons
-	col <- pull(deces_hebdo, colName)
-	y_seasonLabel <- base::min(col)
-	
-	# Graphe
-	p <- ggplot(deces_hebdo) +
-			ggtitle(paste0("Décès standardisés (", titleSuffix,")\n", str_to_title(nomPays))) +
-			theme_bw() +
-			theme(plot.title = element_text(color = "#003366", size = 20, face = "bold",hjust = 0.5))+
-			
-			aes_string(x = "numSemaineAnnee", y = colName) +
-			
-			geom_line(aes(color = annee_coupee_ete), size=1.3) +
-			
-			# Lignes verticales de séparation des saisons
-			geom_vline(xintercept = c(12,25,38,51), linetype = "longdash")+
-			
-			xlab("semaine")+ 
-			scale_x_continuous(breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100))+
-			
-			ylab(paste0("Décès standardisés")) +
-			
-			# La ligne suivante fait bugguer de temps en temps
-			scale_y_continuous(labels = a__f_spaceThousandsSeparator) +
-			
-			geom_text(x=6, y=y_seasonLabel, label="été")+
-			geom_text(x=18, y=y_seasonLabel, label="automne")+
-			geom_text(x=31, y=y_seasonLabel, label="hiver")+
-			geom_text(x=44, y=y_seasonLabel, label="printemps")
-	
-	#
-	# Couleurs de chaque courbe
-	#
-	
-	# Déterminer l'année ayant eu le plus de décès
-	deathMaxIndex <- which.max(col)
-	deathMaxSummerCutYear <- deces_hebdo$annee_coupee_ete[deathMaxIndex]
-	deathMax <- as.integer(col[deathMaxIndex])
-	
-	# Année à gauche du tiret
-	deathMaxSummerCutYear_1 = as.numeric(str_sub(deathMaxSummerCutYear, 1, 4))
-	
-	# Affecter les couleurs pour chaque courbe
-	
-	if(nbLinesFor2013 > 0){
-		# Il y a la courbe pour 2013
-		
-		# Gris, sauf pour les 3 années les plus récentes
-		lineColors <- c('#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#00CC66', '#3399FF','#CC0033')
-		
-		# Forcer la couleur roug pour l'année la pire
-		lineColors[deathMaxSummerCutYear_1 - 2013 + 1] <- '#FF0000' 
-		
-	} else {
-		# Pas de courbe pour 2013
-		
-		# Gris, sauf pour les 3 années les plus récentes
-		lineColors <- c('#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#00CC66', '#3399FF','#CC0033')
-		
-		# Forcer la couleur roug pour l'année la pire
-		lineColors[deathMaxSummerCutYear_1 - 2014 + 1] <- '#FF0000' 
-	}	  
-	
-	p <- p + scale_color_manual(values = lineColors)
-	
-	# Affichage d'un label avec la valeur max
-	
-	if (shallCumul) {
-		
-		# Déterminer l'abscisse du dernier échantillon
-		x_max = max(deces_hebdo$numSemaineAnnee)
-		
-		# Calculer la valeur moyenne pour la dernière semaine
-		y_moy =	deces_hebdo  %>%
-				filter(numSemaineAnnee == x_max) %>%
-				group_by(geo) %>% 
-				summarise(meanNbOfDeaths = mean(!!sym(colName)))
-		
-		y_moy <- as.integer(y_moy[1,2])
-		
-		# Afficher les valeurs
-		p <- p + geom_text(x = x_max, y = deathMax, label = a__f_spaceThousandsSeparator(deathMax), hjust = 1)
-		p <- p + geom_text(x = x_max, y = y_moy, label = a__f_spaceThousandsSeparator(y_moy), hjust = 1)
-		
-	} else {
-		# Pas en mode cumul
-		
-		# Ajouter le smooth
-		p <- p + geom_smooth(formula = y ~ x, method = "loess", color = '#CCCCCC')
-	}
-	
-	
-	#
-	# Dessiner le graphe
-	#
-	plot(p)
-	
-	#
-	# Sauvegarder le graphique
-	#
-	
-	ggsave(pngFileRelPath, width = 11, height = 8, plot = p)
+	# ATTENTION : Pour voir les variables dans le debugger, il faut commenter le tryCatchLog
+	tryCatchLog( {
+				
+				
+				#
+				# Créer le répertoire pour stocker le graphique
+				#
+				
+				# Déterminer le répertoire et le nom de la colonne à tracer (= la tranche d'âge à tracer)
+				
+				if (shallCumul) {
+					
+					K_DIR_GEN_IMG_EUROSTAT_DECES_ANNEE_COUPEE_ETE <- a__f_createDir(file.path(K_DIR_GEN_IMG_EUROSTAT, '/Deces/Hebdo/Std/Deces_Annee_coupee_ete_Cumul'))
+					
+					colName <- paste0("cum_deces_", gsub("-", "_", trancheAge))
+					
+				} else {
+					
+					K_DIR_GEN_IMG_EUROSTAT_DECES_ANNEE_COUPEE_ETE <- a__f_createDir(file.path(K_DIR_GEN_IMG_EUROSTAT, '/Deces/Hebdo/Std/Deces_Annee_coupee_ete'))
+					
+					colName <- paste0("deces_standardises_si_pop_2020_", gsub("-", "_", trancheAge))
+				}
+				
+				
+				repertoire <- paste0(K_DIR_GEN_IMG_EUROSTAT_DECES_ANNEE_COUPEE_ETE, "/", trancheAge,"/")
+				a__f_createDir(repertoire)
+				
+				#Nom du fichier png à générer
+				pngFileRelPath <- paste0(repertoire, nomPays, ".png")
+				
+				cat(paste0("Creation image (", pngFileRelPath,")\n"))
+				
+				#
+				# Générer le graphique
+				#
+				
+				# Déterminer l'ordonnée pour les labels des saisons
+				col <- pull(deces_hebdo, colName)
+				y_seasonLabel <- base::min(col)
+				
+				# Graphe
+				p <- ggplot(deces_hebdo) +
+						ggtitle(paste0("Décès standardisés (", titleSuffix,")\n", str_to_title(nomPays))) +
+						theme_bw() +
+						theme(plot.title = element_text(color = "#003366", size = 20, face = "bold",hjust = 0.5))+
+						
+						aes_string(x = "numSemaineAnnee", y = colName) +
+						
+						geom_line(aes(color = annee_coupee_ete), size=1.3) +
+						
+						# Lignes verticales de séparation des saisons
+						geom_vline(xintercept = c(12,25,38,51), linetype = "longdash")+
+						
+						xlab("semaine")+ 
+						scale_x_continuous(breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100))+
+						
+						ylab(paste0("Décès standardisés")) +
+						
+						# La ligne suivante fait bugguer de temps en temps
+						scale_y_continuous(labels = a__f_spaceThousandsSeparator) +
+						
+						geom_text(x=6, y=y_seasonLabel, label="été")+
+						geom_text(x=18, y=y_seasonLabel, label="automne")+
+						geom_text(x=31, y=y_seasonLabel, label="hiver")+
+						geom_text(x=44, y=y_seasonLabel, label="printemps")
+				
+				#
+				# Couleurs de chaque courbe
+				#
+				
+				# Déterminer l'année ayant eu le plus de décès
+				deathMaxIndex <- which.max(col)
+				deathMaxSummerCutYear <- deces_hebdo$annee_coupee_ete[deathMaxIndex]
+				deathMax <- as.integer(col[deathMaxIndex])
+				
+				# Année à gauche du tiret
+				deathMaxSummerCutYear_1 = as.numeric(str_sub(deathMaxSummerCutYear, 1, 4))
+				
+				# Affecter les couleurs pour chaque courbe
+				
+				if(nbLinesFor2013 > 0){
+					# Il y a la courbe pour 2013
+					
+					# Gris, sauf pour les 3 années les plus récentes
+					lineColors <- c('#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#00CC66', '#3399FF','#CC0033')
+					
+					# Forcer la couleur rouge pour l'année la pire
+					lineColors[deathMaxSummerCutYear_1 - 2013 + 1] <- '#FF0000' 
+					
+				} else {
+					# Pas de courbe pour 2013
+					
+					# Gris, sauf pour les 3 années les plus récentes
+					lineColors <- c('#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#CCCCCC','#00CC66', '#3399FF','#CC0033')
+					
+					# Forcer la couleur rouge pour l'année la pire
+					lineColors[deathMaxSummerCutYear_1 - 2014 + 1] <- '#FF0000' 
+				}	  
+				
+				p <- p + scale_color_manual(values = lineColors)
+				
+				# Affichage d'un label avec la valeur max
+				
+				if (shallCumul) {
+					
+					# Déterminer l'abscisse du dernier échantillon
+					x_max = max(deces_hebdo$numSemaineAnnee)
+					
+					# Calculer la valeur moyenne pour la dernière semaine
+					y_moy =	deces_hebdo  %>%
+							filter(numSemaineAnnee == x_max) %>%
+							group_by(geo) %>% 
+							summarise(meanNbOfDeaths = mean(!!sym(colName)))
+					
+					y_moy <- as.integer(y_moy[1,2])
+					
+					# Afficher les valeurs
+					p <- p + geom_text(x = x_max, y = deathMax, label = a__f_spaceThousandsSeparator(deathMax), hjust = 1)
+					p <- p + geom_text(x = x_max, y = y_moy, label = a__f_spaceThousandsSeparator(y_moy), hjust = 1)
+					
+				} else {
+					# Pas en mode cumul
+					
+					# Ajouter le smooth
+					p <- p + geom_smooth(formula = y ~ x, method = "loess", color = '#CCCCCC')
+				}
+				
+				
+				#
+				# Dessiner le graphe
+				#
+				plot(p)
+				
+				#
+				# Sauvegarder le graphique
+				#
+				
+				ggsave(pngFileRelPath, width = 11, height = 8, plot = p)
+				
+			}, 
+			warning = a__f_warning, 
+			error = a__f_error)
 }
 
 # Calcul des décès cumulés et appel de l'affichage pour chaque tranche d'âge
 a__f_cumul_and_plot_es_deces_hebdo_std_annee_juin <- function(es_deces_standard_pays_semaine) {
 	
+	# ATTENTION : Pour voir les variables dans le debugger, il faut commenter le tryCatchLog
 	tryCatchLog( {
 				
 				# Ajouter la colonne annee_coupee_ete 
@@ -9542,9 +9551,10 @@ a__f_cumul_and_plot_es_deces_hebdo_std_annee_juin <- function(es_deces_standard_
 				a__f_plot_es_deces_hebdo_std_annee_juin(nomPays, "15-24", "15-24 ans", 	    temp2, nbLinesFor2013, TRUE)
 				a__f_plot_es_deces_hebdo_std_annee_juin(nomPays, "total", "Tous âges confondus", 	    temp2, nbLinesFor2013, TRUE)
 				
-			}, 
-			error = a__f_error)
-	
+			 }, 
+			 warning = a__f_warning, 
+			 error = a__f_error)
+			 
 }
 
 ################################################################################
