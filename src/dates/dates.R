31cc8586827fccd64eed54a87a165e70f05216af
JeanGarf <thierryw777@gmail.com>
2022-10-31 18:17:11 +0100

    Ajout de fonctions pour les dates

--- src/dates/dates.R
+++ src/dates/dates.R
@@ -15,3 +15,25 @@ a__f_get_period <- function(deces_date, nb_months_by_period, start_date) {
 	period_nb <- as.integer(nb_days / nb_days_by_period) + 1
 }
 
+
+################################################################################
+# Convertir une Date en n° de semaine Eurostat (20xxWyy)
+################################################################################
+a__f_get_eurostat_week_from_date <- function(date) {
+	
+	paste0(isoyear(date),
+			"W", 
+			as.integer(isoweek(date)/10), 
+			isoweek(date) - as.integer(isoweek(date)/10)*10)
+}
+
+################################################################################
+# Convertir un n° de semaine Eurostat (20xxWyy) en la Date du Lundi de cette semaine
+################################################################################
+a__f_get_date_from_eurostat_week <- function(eurostat_week) {
+	
+	# Suffixer la semaine avec "_1" (pour Lundi)
+	# TODO TW : Ne marche pas pour "2015W53", je ne sais pas pourquoi
+	as.Date(paste0(eurostat_week, "_1"), "%YW%W_%u")
+}
+
